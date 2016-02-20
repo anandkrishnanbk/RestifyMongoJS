c268485ad2f9369a88b2b148833d3615bd85c946
anandkrishnankutty <anand.krishnankutty@bcbsnc.com>
2016-02-20 18:27:19 -0500

    initial

---
+++ client.js
@@ -0,0 +1,71 @@
+var restify=require('restify');
+var server=require('./server');
+
+var client =restify.createJSONClient({url:'http://localhost:3000'});
+
+var testProducts = {"id" : "105", "gender" : "Female", "first_name" : "Nandana",
+    "last_name" : "Soman", "email" : "nandana@gmail.com",
+    "ip_address" : "255.45.255.45" };
+
+client.post('/products',testProducts,function(err,req,resp,product)
+{
+    if(err)
+    {
+       console.log('error occured'+err);
+    }
+    else{
+
+        console.log('product saved-'+product.email);
+    }
+});
+client.get('/products', function (err, req, res, products)
+{
+    if (err) {
+        console.log('get-error occured-'+err);
+    } else {
+        //console.log("Total products " + products.length);
+        //console.log('All products >>>>>>>');
+        //console.log(products);
+    }
+});
+client.put('/products/109/Geeta',function(err,req,res,status)
+{
+    if(err)
+    {
+        console.log('put-err occured-'+err);
+    }
+    else
+    {
+        console.log('put status-'+status);
+    }
+
+
+});
+client.del('/products/101',function(err,req,res,status)
+{
+    if(err)
+    {
+        console.log('del-err occured-'+err);
+    }else
+    {
+        console.log("del status-"+status);
+    }
+
+});
+
+client.get('/products/105',function(err,req,res,data)
+{
+    if(err)
+    {
+       console.log('get one err-'+err);
+    }
+    else
+    {
+        console.log('get one status-'+data.first_name);
+
+    }
+
+
+});
+
+module.exports= server;
---
+++ server.js
@@ -0,0 +1,75 @@
+var restify=require('restify');
+var mongojs=require('mongojs');
+
+var db =mongojs('productdb',['products'])
+
+var server = restify.createServer();
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.listen(3000,function()
+{
+    console.log('server started %s',server.url);
+});
+server.get('/products', function (req, res, next) {
+    db.products.find(function(err,products){
+        res.writeHead(200, {
+            'Content-Type': 'application/json; charset=utf-8'
+        });
+        res.end(JSON.stringify(products));
+    });
+    //res.send("You will see all the products in the colection with this end point");
+    next();
+});
+server.get('/products/:id',function(req,res,next)
+{
+    db.products.findOne({id:req.params.id},function(err,data)
+    {
+        res.writeHead(200,{'Content-Type':'application/json;charset=utf-8'});
+        res.end(JSON.stringify(data));
+
+    });
+    next();
+});
+server.post('/products',function(req,res,next){
+
+    var product = req.params;
+    db.products.save(product,function(err,data)
+    {
+        res.writeHead(200,{'Content-Type':'application/json;charset=utf-8'});
+        res.end(JSON.stringify(data));
+
+    });
+    next();
+
+});
+server.put('/products/:id/:lastName',function(req,res,next)
+{
+     var id=req.params.id;
+     var lastName=req.params.lastName;
+     db.products.update({id:id},{$set:{last_name:lastName}},function(err,data)
+     {
+         res.writeHead(200,{'Content-Type':'application/json,charset=utf-8'});
+         res.end(JSON.stringify(data));
+     });
+    next();
+});
+server.del('/products/:id',function(req,res,next)
+{
+    db.products.remove({id:req.params.id},function(err,data)
+    {
+        if(!err)
+        {
+            res.writeHead(200,{'Content-Type':'application/json;charset=utf-8'});
+            res.end(JSON.stringify(true));
+        }
+        else
+        {
+            console.log('err-'+err);
+        }
+
+    });
+    next();
+
+});
